46e888c573aaa00fba251e9a15c42b441798ba05
Njeri Gachoka <njerig00@gmail.com>
2019-04-22 23:29:13 -0400

    keep cookie headers

--- server.js
+++ server.js
@@ -26,8 +26,6 @@ cors_proxy.createServer({
   requireHeader: ['origin'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
-    'cookie',
-    'coookie2',
     // Strip Heroku-specific headers
     'x-heroku-queue-wait-time',
     'x-heroku-queue-depth',
